cdb5f4778a0ca3ece6cd374f095a8cc8e36a3e91
nohseungrae <ca10205@naver.com>
2019-12-07 13:15:11 +0900

    logout

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
+const saltRounds = 10;
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -25,7 +28,53 @@ const userSchema = mongoose.Schema({
     type: Number
   }
 });
+//------------------------------------
+userSchema.pre("save", function(next) {
+  let user = this;
+  console.log("enter2");
+  if (user.isModified("password")) {
+    console.log("enter1");
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err);
 
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+//------------------------------------
+userSchema.methods.comparePassword = function(plainPassword, callback) {
+  bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+    if (err) return callback(err);
+    callback(null, isMatch);
+  });
+};
+//------------------------------------
+userSchema.methods.generateToken = function(callback) {
+  let user = this;
+  let token = jwt.sign(user._id.toHexString(), "secret");
+  user.token = token;
+  user.save((err, user) => {
+    if (err) return callback(err);
+    callback(null, user);
+  });
+};
+//------------------------------------
+userSchema.statics.findByToken = function(token, callback) {
+  let user = this;
+
+  jwt.verify(token, "secret", function(err, decode) {
+    user.findOne({ _id: decode, token: token }, function(err, user) {
+      if (err) return callback(err);
+      callback(null, user);
+    });
+  });
+};
 const User = mongoose.model("User", userSchema);
 
 export default User;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,68 @@
 import express from "express";
 import user from "../models/User";
+import Auth from "../middlewares/auth";
+
 const userRouter = express.Router();
 
-userRouter.get("/auth");
-userRouter.get("/login");
+userRouter.get("/auth", Auth, (req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    role: req.user.role
+  });
+});
+//------------------------------------------
+userRouter.post("/login", (req, res) => {
+  //find the email
+  user.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "인증실패 이메일을 찾을 수 없습니다."
+      });
+    //comparePassword
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다."
+        });
+      }
+      //generateToken
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res
+          .cookie("auth_token", user.token)
+          .status(200)
+          .json({ loginSuccess: true });
+      });
+    });
+  });
+});
+//------------------------------------------
 userRouter.post("/register", (req, res) => {
   const User = new user(req.body);
-
+  console.log(User);
   User.save((err, userData) => {
-    if (err) return res.json({ success: false, err });
+    if (err) return res.json({ success: "안되잖아", err });
 
     return res.status(200).json({ success: true, userData });
   });
 });
-userRouter.get("/logout");
+//------------------------------------------
+userRouter.get("/logout", Auth, (req, res) => {
+  user.findOneAndUpdate(
+    { _id: req.user._id },
+    { token: "" },
+    (err, userData) => {
+      if (err) return res.json({ success: false, err });
+      return res.status(200).json({
+        success: true
+      });
+    }
+  );
+});
 
 export default userRouter;
